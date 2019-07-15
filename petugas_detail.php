28e547a46e96de5bc88c2cbdd1d2e93c771a7ffe
yohantoro <yohantoro.prakasita@gmail.com>
2019-07-15 15:52:20 +0700

    - tampilkan pesan error jika query gagal dijalankan - tambah link delete

--- petugas_detail.php
+++ petugas_detail.php
@@ -1,31 +1,37 @@
 <?php
 require_once 'layout_header.php';
 
-$id = $_GET['id'];
-$query = "SELECT * FROM petugas WHERE `id` = " . $id;
+/**
+ * Jika ada request $_GET['id'], @var $id diisi $_GET['id']
+ * Jika tidak, diisi angka 0 (nol)
+ */
+$id = isset($_GET['id']) ? $_GET['id'] : 0;
 
+$query = "SELECT * FROM petugas WHERE `id` = " . $id;
 $result = mysqli_query($con, $query);
+
+if (!$result) { // jika query gagal
+    /* Aplikasi berhenti dan menampilkan error */
+    die(mysqli_error($con));
+}
 
 ?>
 
 <div class="petugas-list">
     <h1>Detail Petugas</h1>
 
-    <p>
-        <a href="petugas_list.php" class="btn btn-secondary">Kembali ke Daftar Petugas</a>
-        <a href="petugas_form.php?id=<?= $id ?>" class="btn btn-warning">Ubah</a>
-        <a href="#" class="btn btn-danger">Hapus</a>
-    </p>
-
     <?php if (mysqli_num_rows($result) <= 0): ?>
-
         <div class="alert alert-danger" role="alert">
             Data tidak ditemukan!
         </div>
-
     <?php else:
         $data = mysqli_fetch_assoc($result);
     ?>
+        <p>
+            <a href="petugas_list.php" class="btn btn-secondary">Kembali ke Daftar Petugas</a>
+            <a href="petugas_form.php?id=<?= $id ?>" class="btn btn-warning">Ubah</a>
+            <a href="petugas_act_delete.php?id=<?= $data['id'] ?>" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure?')">Hapus</a>
+        </p>
 
         <table class="table table-bordered">
             <tbody>

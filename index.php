146a395637d0668e447055296703caf0e8d94bdb
yohantoro <yohantoro.prakasita@gmail.com>
2019-07-15 09:13:30 +0700

    halaman index aplikasi

--- index.php
+++ index.php
@@ -1,53 +1,10 @@
-<!doctype html>
-<html lang="en">
-<head>
+<?php require 'layout_header.php'; ?>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<div class="starter-template">
 
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css">
-    <link rel="stylesheet" href="assets/app/style.css">
+    <h1>Bootstrap starter template</h1>
+    <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
 
-    <title>Arsip</title>
+</div>
 
-</head>
-
-<body>
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Beranda <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Surat Masuk</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Surat Keluar</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Logout</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-
-    <main role="main" class="container">
-        <div class="starter-template">
-
-            <h1>Bootstrap starter template</h1>
-            <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-
-        </div>
-    </main><!-- /.container -->
-
-    <script src="assets/jquery/jquery-3.3.1.min.js"></script>
-    <script src="assets/popper/popper.min.js"></script>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-</body>
-</html>
+<?php require 'layout_footer.php'; ?>

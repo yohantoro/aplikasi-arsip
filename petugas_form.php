df979f557323c2531bdb63f0fa660557ea1a8c17
yohantoro <yohantoro.prakasita@gmail.com>
2019-07-16 23:45:28 +0700

    kolom input dibungkus dengan card

--- petugas_form.php
+++ petugas_form.php
@@ -37,20 +37,24 @@ if (mysqli_num_rows($result) > 0) {
     </p>
 
     <form action="<?= $action ?>" method="post">
-        <input type="hidden" name="id" value="<?= $id ?>">
+        <div class="card">
+            <div class="card-body">
+                <input type="hidden" name="id" value="<?= $id ?>">
 
-        <div class="form-group">
-            <label for="nama-depan">Nama Depan</label>
-            <input type="text" name="nama_depan" class="form-control" id="nama-depan" aria-describedby="nama-depan" value="<?= $namaDepan ?>" required>
+                <div class="form-group">
+                    <label for="nama-depan">Nama Depan</label>
+                    <input type="text" name="nama_depan" class="form-control" id="nama-depan" aria-describedby="nama-depan" value="<?= $namaDepan ?>" required>
+                </div>
+
+                <div class="form-group">
+                    <label for="nama-belakang">Nama Belakang</label>
+                    <input type="text" name="nama_belakang" class="form-control" id="nama-belakang" aria-describedby="nama-belakang" value="<?= $namaBelakang ?>">
+                    <small id="nama-belakang-help" class="form-text text-muted">Nama belakang boleh dikosongi.</small>
+                </div>
+                <button type="submit" name="button" class="btn btn-info">Simpan</button>
+            </div>
         </div>
 
-        <div class="form-group">
-            <label for="nama-belakang">Nama Belakang</label>
-            <input type="text" name="nama_belakang" class="form-control" id="nama-belakang" aria-describedby="nama-belakang" value="<?= $namaBelakang ?>">
-            <small id="nama-belakang-help" class="form-text text-muted">Nama belakang boleh dikosongi.</small>
-        </div>
-
-        <button type="submit" name="button" class="btn btn-info">Simpan</button>
     </form>
 </div>
 

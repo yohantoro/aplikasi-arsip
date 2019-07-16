8a85245d9bfa90af71dbbc7f2a9639b968ee2809
yohantoro <yohantoro.prakasita@gmail.com>
2019-07-16 15:40:41 +0700

    masukkan nav items ke dalam container

--- layout_header.php
+++ layout_header.php
@@ -20,32 +20,34 @@ include 'config/connection.php';
 
 <body>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+        <div class="container">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php">Beranda <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Surat Masuk</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Surat Keluar</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="dropdown-referensi" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Referensi</a>
-                    <div class="dropdown-menu" aria-labelledby="dropdown-referensi">
-                        <a class="dropdown-item" href="petugas_list.php">Petugas</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Logout</a>
-                </li>
-            </ul>
+            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Beranda <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Surat Masuk</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Surat Keluar</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown-referensi" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Referensi</a>
+                        <div class="dropdown-menu" aria-labelledby="dropdown-referensi">
+                            <a class="dropdown-item" href="petugas_list.php">Petugas</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Logout</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
 

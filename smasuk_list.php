6a527092dc3af54933d2c2ef4ddc7cb788d7790e
yohantoro <yohantoro.prakasita@gmail.com>
2019-07-18 14:08:51 +0700

    list dan detail

---
+++ smasuk_list.php
@@ -0,0 +1,55 @@
+<?php
+
+include 'layout_header.php';
+
+$query = "SELECT * FROM surat_masuk";
+$hasil = mysqli_query($con, $query);
+
+if ($hasil == false) {
+    die('Query gagal dijalankan. Error: ' . mysqli_error($con));
+}
+
+?>
+
+<h1>Daftar Surat Masuk</h1>
+
+<p>
+    <a href="smasuk_form.php" class="btn btn-primary">Buat Data Baru</a>
+</p>
+
+<?php if (mysqli_num_rows($hasil) <= 0): ?>
+
+    <div class="alert alert-danger" role="alert">
+        Data kosong
+    </div>
+
+<?php else: ?>
+
+    <table class="table table-bordered">
+        <thead class="thead-dark">
+            <th>No Agenda</th>
+            <th>No Surat</th>
+            <th>Tgl. Kirim</th>
+            <th>Pengirim</th>
+            <th>Aksi</th>
+        </thead>
+        <tbody>
+            <?php while ($row = mysqli_fetch_assoc($hasil)) : ?>
+                <tr>
+                    <td><?php echo $row['no_agenda'] ?></td>
+                    <td><?php echo $row['no_surat'] ?></td>
+                    <td><?php echo $row['tgl_kirim'] ?></td>
+                    <td><?php echo $row['pengirim'] ?></td>
+                    <td>
+                        <a href="smasuk_detail.php?id=<?= $row['id'] ?>" class="btn btn-info">Detail</a>
+                        <a href="#" class="btn btn-warning">Ubah</a>
+                        <a href="#" class="btn btn-danger">Hapus</a>
+                    </td>
+                </tr>
+            <?php endwhile; ?>
+        </tbody>
+    </table>
+
+<?php endif; ?>
+
+<?php include 'layout_footer.php'; ?>
